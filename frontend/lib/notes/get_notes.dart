6429448e08b823ce1d3fb721db911112d8e4d166
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-14 22:50:27 +0700

    feat/create_notes

--- frontend/lib/notes/get_notes.dart
+++ frontend/lib/notes/get_notes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class GetNotesScreen extends StatefulWidget {
   const GetNotesScreen({super.key});
